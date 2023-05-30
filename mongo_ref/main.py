8e1738d296042a54a2ef9d954ec82e205a2224ac
temaput <putilkin@gmail.com>
2023-05-30 00:08:03 -0300

    cleanup

--- mongo_ref/main.py
+++ mongo_ref/main.py
@@ -13,6 +13,7 @@ db = client.college
 
 
 class PyObjectId(ObjectId):
+
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
@@ -68,25 +69,29 @@ class UpdateStudentModel(BaseModel):
         }
 
 
-@app.post("/", response_description="Add new student", response_model=StudentModel)
+@app.post("/",
+          response_description="Add new student",
+          response_model=StudentModel)
 async def create_student(student: StudentModel = Body(...)):
     student = jsonable_encoder(student)
     new_student = await db["students"].insert_one(student)
-    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+    created_student = await db["students"].find_one(
+        {"_id": new_student.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED,
+                        content=created_student)
 
 
-@app.get(
-    "/", response_description="List all students", response_model=List[StudentModel]
-)
+@app.get("/",
+         response_description="List all students",
+         response_model=List[StudentModel])
 async def list_students():
     students = await db["students"].find().to_list(1000)
     return students
 
 
-@app.get(
-    "/{id}", response_description="Get a single student", response_model=StudentModel
-)
+@app.get("/{id}",
+         response_description="Get a single student",
+         response_model=StudentModel)
 async def show_student(id: str):
     if (student := await db["students"].find_one({"_id": id})) is not None:
         return student
@@ -94,20 +99,23 @@ async def show_student(id: str):
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
 
 
-@app.put("/{id}", response_description="Update a student", response_model=StudentModel)
+@app.put("/{id}",
+         response_description="Update a student",
+         response_model=StudentModel)
 async def update_student(id: str, student: UpdateStudentModel = Body(...)):
     student = {k: v for k, v in student.dict().items() if v is not None}
 
     if len(student) >= 1:
-        update_result = await db["students"].update_one({"_id": id}, {"$set": student})
+        update_result = await db["students"].update_one({"_id": id},
+                                                        {"$set": student})
 
         if update_result.modified_count == 1:
-            if (
-                updated_student := await db["students"].find_one({"_id": id})
-            ) is not None:
+            if (updated_student := await
+                    db["students"].find_one({"_id": id})) is not None:
                 return updated_student
 
-    if (existing_student := await db["students"].find_one({"_id": id})) is not None:
+    if (existing_student := await db["students"].find_one({"_id":
+                                                           id})) is not None:
         return existing_student
 
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
